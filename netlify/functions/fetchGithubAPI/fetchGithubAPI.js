9e766790aa8ce2c2fa3b69c623233fff5b644cfe
Rawle Juglal <rawledjuglal@gmail.com>
2023-11-28 15:29:59 -0600

    building serverless function
    for netlify to use protected env variable

--- netlify/functions/fetchGithubAPI/fetchGithubAPI.js
+++ netlify/functions/fetchGithubAPI/fetchGithubAPI.js
@@ -1,10 +1,26 @@
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
-const handler = async (event) => {
+//https://recharge-portfolio.netlify.app.netlify/functions/fetchGithubAPI
+import { Octokit } from "octokit";
+
+const octokit = new Octokit({
+    auth: process.env.Github_API
+  })
+
+const getRepos = async (event) => {
   try {
-    const subject = event.queryStringParameters.name || 'World'
+    const repos = await octokit.request('GET /users/{username}/repos', {
+      username: 'RawleJuglal',
+      per_page:100,
+      headers: {
+        'X-GitHub-Api-Version': '2022-11-28'
+      }
+    })
+    .then(res => {
+     return res.data.filter(ele => !ele.archived)
+    })
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
+      body: JSON.stringify({ reply: repos.data }),
       // // more keys you can return:
       // headers: { "headerName": "headerValue", ... },
       // isBase64Encoded: true,
@@ -14,4 +30,83 @@ const handler = async (event) => {
   }
 }
 
-module.exports = { handler }
+const getLanguages = async (event) => {
+  try {
+    const reservedWord =  await octokit.request('GET /repos/{username}/{repo}/languages', {
+        username:'RawleJuglal',
+        repo:ele.name,
+        headers: {
+            'X-GitHub-Api-Version': '2022-11-28'
+        }
+    })
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ reply: reservedWord.data }),
+      // // more keys you can return:
+      // headers: { "headerName": "headerValue", ... },
+      // isBase64Encoded: true,
+    }
+  } catch (error) {
+    return { statusCode: 500, body: error.toString() }
+  }
+}
+
+const getDependencies = async (event) => {
+  try {
+    const dependecies = await fetch(`https://raw.githubusercontent.com/RawleJuglal/${ele.name}/master/package.json`)
+        .then(res => {
+            if(res.ok){
+                return res.json()
+            } else {
+                return null;
+            }
+        })
+        .then(data => {
+            if(data){
+                const figmaDesigns = [
+                    'reactstatic', 
+                    'digitalbusinesscard',
+                    'airbnbclone',
+                    'traveljournal',
+                    'memegenerator', 
+                    'tenzies',
+                    'quizzical',
+                    'moviewatchlist',
+                    'scoreboard',
+                    'passwordgenerator',
+                    'unitconverter',
+                    'oldagram',
+                    'restaurant',
+                    'hotspots',
+                    'Learningjournal',
+                    'homereadyrenovations',
+                    'pollyglot',
+                ]
+
+                let currDep = [];
+                if(data.dependencies){
+                    currDep = [...Object.keys(data.dependencies)]
+                }
+                if(data.devDependencies){
+                    currDep = [...currDep, ...Object.keys(data.devDependencies)]
+                }
+                currDep = [...currDep, 'html5', 'css', 'javascript', 'git']
+                if(figmaDesigns.includes(data.name)){
+                    currDep = [...currDep, 'figma']
+                }
+                return currDep;
+            }
+        })
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ reply: dependecies.data }),
+      // // more keys you can return:
+      // headers: { "headerName": "headerValue", ... },
+      // isBase64Encoded: true,
+    }
+  } catch (error) {
+    return { statusCode: 500, body: error.toString() }
+  }
+}
+
+module.exports = { getRepos, getLanguages, getDependencies  }
--- projects.js
+++ projects.js
@@ -1,83 +1,86 @@
-import { Octokit } from "octokit";
-import { process } from './env'
+// import { Octokit } from "octokit";
 
-const octokit = new Octokit({
-    auth: process.env.Github_API
-  })
+// const octokit = new Octokit({
+//     auth: process.env.Github_API
+//   })
 
-const repos = await octokit.request('GET /users/{username}/repos', {
-    username: 'RawleJuglal',
-    per_page:100,
-    headers: {
-      'X-GitHub-Api-Version': '2022-11-28'
-    }
-  })
-  .then(res => {
-   return res.data.filter(ele => !ele.archived)
-  })
+// const repos = await octokit.request('GET /users/{username}/repos', {
+//     username: 'RawleJuglal',
+//     per_page:100,
+//     headers: {
+//       'X-GitHub-Api-Version': '2022-11-28'
+//     }
+//   })
+//   .then(res => {
+//    return res.data.filter(ele => !ele.archived)
+//   })
 
-async function getLanguages(ele){
-    const reservedWord =  await octokit.request('GET /repos/{username}/{repo}/languages', {
-        username:'RawleJuglal',
-        repo:ele.name,
-        headers: {
-            'X-GitHub-Api-Version': '2022-11-28'
-        }
-    })
-    return reservedWord.data
-}
+import {getRepos} from './netlify/functions/fetchGithubAPI'
 
-async function getDependencies(ele){
-    try {
-        const dependecies = await fetch(`https://raw.githubusercontent.com/RawleJuglal/${ele.name}/master/package.json`)
-        .then(res => {
-            if(res.ok){
-                return res.json()
-            } else {
-                return null;
-            }
-        })
-        .then(data => {
-            if(data){
-                const figmaDesigns = [
-                    'reactstatic', 
-                    'digitalbusinesscard',
-                    'airbnbclone',
-                    'traveljournal',
-                    'memegenerator', 
-                    'tenzies',
-                    'quizzical',
-                    'moviewatchlist',
-                    'scoreboard',
-                    'passwordgenerator',
-                    'unitconverter',
-                    'oldagram',
-                    'restaurant',
-                    'hotspots',
-                    'Learningjournal',
-                    'homereadyrenovations',
-                    'pollyglot',
-                ]
+const repos = getRepos()
 
-                let currDep = [];
-                if(data.dependencies){
-                    currDep = [...Object.keys(data.dependencies)]
-                }
-                if(data.devDependencies){
-                    currDep = [...currDep, ...Object.keys(data.devDependencies)]
-                }
-                currDep = [...currDep, 'html5', 'css', 'javascript', 'git']
-                if(figmaDesigns.includes(data.name)){
-                    currDep = [...currDep, 'figma']
-                }
-                return currDep;
-            }
-        })
-        return dependecies;
-    } catch (error) {
-       console.log('im in the error')
-    }
-}
+// async function getLanguages(ele){
+//     const reservedWord =  await octokit.request('GET /repos/{username}/{repo}/languages', {
+//         username:'RawleJuglal',
+//         repo:ele.name,
+//         headers: {
+//             'X-GitHub-Api-Version': '2022-11-28'
+//         }
+//     })
+//     return reservedWord.data
+// }
+
+// async function getDependencies(ele){
+//     try {
+//         const dependecies = await fetch(`https://raw.githubusercontent.com/RawleJuglal/${ele.name}/master/package.json`)
+//         .then(res => {
+//             if(res.ok){
+//                 return res.json()
+//             } else {
+//                 return null;
+//             }
+//         })
+//         .then(data => {
+//             if(data){
+//                 const figmaDesigns = [
+//                     'reactstatic', 
+//                     'digitalbusinesscard',
+//                     'airbnbclone',
+//                     'traveljournal',
+//                     'memegenerator', 
+//                     'tenzies',
+//                     'quizzical',
+//                     'moviewatchlist',
+//                     'scoreboard',
+//                     'passwordgenerator',
+//                     'unitconverter',
+//                     'oldagram',
+//                     'restaurant',
+//                     'hotspots',
+//                     'Learningjournal',
+//                     'homereadyrenovations',
+//                     'pollyglot',
+//                 ]
+
+//                 let currDep = [];
+//                 if(data.dependencies){
+//                     currDep = [...Object.keys(data.dependencies)]
+//                 }
+//                 if(data.devDependencies){
+//                     currDep = [...currDep, ...Object.keys(data.devDependencies)]
+//                 }
+//                 currDep = [...currDep, 'html5', 'css', 'javascript', 'git']
+//                 if(figmaDesigns.includes(data.name)){
+//                     currDep = [...currDep, 'figma']
+//                 }
+//                 return currDep;
+//             }
+//         })
+//         return dependecies;
+//     } catch (error) {
+//        console.log('im in the error')
+//     }
+// }
 
 function decodeName(codedName){
     return codedName.replace(/([a-z0-9])([A-Z])/g, '$1 $2');
