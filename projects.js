2fd210275786f8f350784ce2dddcd7c28f42b88d
raygdev <101662915+raygdev@users.noreply.github.com>
2023-12-03 17:22:38 -0500

    Reformat file

--- projects.js
+++ projects.js
@@ -1,17 +1,14 @@
 import { BASE_URL } from "./baseURL"
 
-
 async function getRepos() {
-  const res = await fetch(
-    `${BASE_URL}/.netlify/functions/fetchGithubRepos`
-  )
+  const res = await fetch(`${BASE_URL}/.netlify/functions/fetchGithubRepos`)
   const data = await res.json()
   return data.reply
 }
 
 async function getLanguages(repoName) {
-    //we are going to fetch from the endpoint in the netlify functions
-    //pass in the repo name instead of the whole repo. It's all we need
+  //we are going to fetch from the endpoint in the netlify functions
+  //pass in the repo name instead of the whole repo. It's all we need
   const res = await fetch(
     `${BASE_URL}/.netlify/functions/fetchRepoLanguages?reponame=${repoName}`
   )
@@ -19,7 +16,6 @@ async function getLanguages(repoName) {
   //reply is the resolved body response from netlify function
   return languages.reply
 }
-
 
 // async function getLanguages(ele){
 //     const reservedWord =  await octokit.request('GET /repos/{username}/{repo}/languages', {
@@ -32,84 +28,86 @@ async function getLanguages(repoName) {
 //     return reservedWord.data
 // }
 
-async function getDependencies(ele){
-    try {
-        const dependecies = await fetch(`https://raw.githubusercontent.com/RawleJuglal/${ele.name}/master/package.json`)
-        .then(res => {
-            if(res.ok){
-                return res.json()
-            } else {
-                return null;
-            }
-        })
-        .then(data => {
-            if(data){
-                const figmaDesigns = [
-                    'reactstatic', 
-                    'digitalbusinesscard',
-                    'airbnbclone',
-                    'traveljournal',
-                    'memegenerator', 
-                    'tenzies',
-                    'quizzical',
-                    'moviewatchlist',
-                    'scoreboard',
-                    'passwordgenerator',
-                    'unitconverter',
-                    'oldagram',
-                    'restaurant',
-                    'hotspots',
-                    'Learningjournal',
-                    'homereadyrenovations',
-                    'pollyglot',
-                ]
-
-                let currDep = [];
-                if(data.dependencies){
-                    currDep = [...Object.keys(data.dependencies)]
-                }
-                if(data.devDependencies){
-                    currDep = [...currDep, ...Object.keys(data.devDependencies)]
-                }
-                currDep = [...currDep, 'html5', 'css', 'javascript', 'git']
-                if(figmaDesigns.includes(data.name)){
-                    currDep = [...currDep, 'figma']
-                }
-                return currDep;
-            } else {
-                return ['html5', 'css', 'javascript', 'git']
-            }
-        })
-        return dependecies;
-    } catch (error) {
-       console.log('im in the error')
-    }
-}
-
-function decodeName(codedName){
-    return codedName.replace(/([a-z0-9])([A-Z])/g, '$1 $2');
-}
-
-const usefulInfo = async () => { 
-    const repos = await getRepos()
-    let usefulInformationArr = repos.map(async ele => {
-        return {
-            id:ele.id,
-            name: decodeName(ele.name),
-            createdAt: ele.created_at,
-            githubLocation:ele.html_url,
-            languages: await getLanguages(ele.name),
-            liveAt:ele.homepage,
-            imageUrl: `https://raw.githubusercontent.com/RawleJuglal/${ele.name}/master/src/assets/screengrab/screengrab.png`,
-            dependencies: await getDependencies(ele)
+async function getDependencies(ele) {
+  try {
+    const dependecies = await fetch(
+      `https://raw.githubusercontent.com/RawleJuglal/${ele.name}/master/package.json`
+    )
+      .then(res => {
+        if (res.ok) {
+          return res.json()
+        } else {
+          return null
         }
-    })
-    return await Promise.all(usefulInformationArr)
+      })
+      .then(data => {
+        if (data) {
+          const figmaDesigns = [
+            "reactstatic",
+            "digitalbusinesscard",
+            "airbnbclone",
+            "traveljournal",
+            "memegenerator",
+            "tenzies",
+            "quizzical",
+            "moviewatchlist",
+            "scoreboard",
+            "passwordgenerator",
+            "unitconverter",
+            "oldagram",
+            "restaurant",
+            "hotspots",
+            "Learningjournal",
+            "homereadyrenovations",
+            "pollyglot"
+          ]
+
+          let currDep = []
+          if (data.dependencies) {
+            currDep = [...Object.keys(data.dependencies)]
+          }
+          if (data.devDependencies) {
+            currDep = [...currDep, ...Object.keys(data.devDependencies)]
+          }
+          currDep = [...currDep, "html5", "css", "javascript", "git"]
+          if (figmaDesigns.includes(data.name)) {
+            currDep = [...currDep, "figma"]
+          }
+          return currDep
+        } else {
+          return ["html5", "css", "javascript", "git"]
+        }
+      })
+    return dependecies
+  } catch (error) {
+    console.log("im in the error")
+  }
+}
+
+function decodeName(codedName) {
+  return codedName.replace(/([a-z0-9])([A-Z])/g, "$1 $2")
+}
+
+const usefulInfo = async () => {
+  const repos = await getRepos()
+  let usefulInformationArr = repos.map(async ele => {
+    return {
+      id: ele.id,
+      name: decodeName(ele.name),
+      createdAt: ele.created_at,
+      githubLocation: ele.html_url,
+      languages: await getLanguages(ele.name),
+      liveAt: ele.homepage,
+      imageUrl: `https://raw.githubusercontent.com/RawleJuglal/${ele.name}/master/src/assets/screengrab/screengrab.png`,
+      dependencies: await getDependencies(ele)
+    }
+  })
+  return await Promise.all(usefulInformationArr)
 }
 
 const repoInfo = await usefulInfo()
 
-export {repoInfo as  usefulInfo }
+export { repoInfo as usefulInfo }
 
 // import {  getLanguages } from './netlify/functions/fetchGithubAPI/fetchGithubAPI.cjs'
 // import { Octokit } from "octokit";
