2e72e81ae62a31744cce1a144bc280dc10b04da5
raygdev <101662915+raygdev@users.noreply.github.com>
2023-12-03 17:22:14 -0500

    Reformat file

--- baseURL.js
+++ baseURL.js
@@ -1,10 +1,9 @@
-let BASE_URL;
+let BASE_URL
 //switches out the base url if NODE_ENV === production
-if(import.meta.env.PROD) {
-    BASE_URL = "https://recharge-portfolio.netlify.app/"
+if (import.meta.env.PROD) {
+  BASE_URL = "https://recharge-portfolio.netlify.app/"
 } else {
-    BASE_URL = "http://localhost:8888"
+  BASE_URL = "http://localhost:8888"
 }
-
 
 export { BASE_URL }
