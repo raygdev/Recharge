bf5aa95c7eee7ea8191ccd71567dff5057dd5866
raygdev <101662915+raygdev@users.noreply.github.com>
2023-12-03 15:57:00 -0500

    Switch out baseURL for prod

---
+++ baseURL.js
@@ -0,0 +1,10 @@
+let BASE_URL;
+//switches out the base url if NODE_ENV === production
+if(import.meta.env.PROD) {
+    BASE_URL = "https://recharge-portfolio.netlify.app"
+} else {
+    BASE_URL = "http://localhost:8888"
+}
+
+
+export { BASE_URL }
