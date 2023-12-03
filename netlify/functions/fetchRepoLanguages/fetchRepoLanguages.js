62a0b9eea8a554e40a2097ff7bcf896306960a3d
raygdev <101662915+raygdev@users.noreply.github.com>
2023-12-03 15:58:20 -0500

    Add enpoint for languages

---
+++ netlify/functions/fetchRepoLanguages/fetchRepoLanguages.js
@@ -0,0 +1,40 @@
+import { app } from "../../../octokitConfig.js"
+
+
+export const handler = async event => {
+
+  try {
+    const repoName = event.queryStringParameters?.reponame
+
+    /**
+     * ensure that a query was passed for the repo name
+     */
+
+    if (!repoName) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ message: "A reponame query is required" })
+      }
+    }
+    const reservedWord = await app.request(
+      "GET /repos/{username}/{repo}/languages",
+      {
+        username: "RawleJuglal",
+        repo: repoName,
+        headers: {
+          "X-GitHub-Api-Version": "2022-11-28"
+        }
+      }
+    )
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ reply: reservedWord.data })
+      // // more keys you can return:
+      // headers: { "headerName": "headerValue", ... },
+      // isBase64Encoded: true,
+    }
+  } catch (error) {
+
+    return { statusCode: 500, body: error.toString() }
+  }
+}
